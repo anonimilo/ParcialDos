aeeaea8ac2f80772e85f82c0161b5acc05412a61
Camilo Restrepo Obando <camiastro6300@gmail.com>
2018-05-25 15:41:54 -0500

    Holi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ engines= require('consolidate'),
 hbs=require('handlebars');
 
 var app= express();
- 
+ fs.req('fs');
 app.engine('hbs',engines.handlebars);
 
 app.set('views','./views');
@@ -11,6 +11,19 @@ app.set('view engine','hbs');
 app.use(express.static('public'));
 
 app.listen(1234);
+
+
+
+var visitas= "",
+Inicio=0;
+var holi;
+fs.readFile('visitas.txt', function read(err,data){
+
+    if (err){
+       throw err;
+    }
+    holi=data;
+});
 
 app.get('/',(req,res)=>{
 
@@ -26,8 +39,6 @@ app.get('/Uno',(req,res)=>{
     res.render("Uno");
     
     });
-
-
 
 
  app.get('/Dos',(req,res)=>{
